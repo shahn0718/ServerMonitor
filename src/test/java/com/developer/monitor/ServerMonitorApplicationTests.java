7306dff43dc0cc2292f234c69217fb2d2ce11743
안상형 <shahn0718@gmail.com>
2023-04-14 15:56:43 +0900

    서버모니터링작업

---
+++ src/test/java/com/developer/monitor/ServerMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
